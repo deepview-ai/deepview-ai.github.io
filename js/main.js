5f6029db35c6362050f787bbe3aca43f827365c8
Paulo <prsilvaruiz@gmail.com>
2018-11-25 21:40:12 -0300

    atualização Paulo 25_11

--- js/main.js
+++ js/main.js
@@ -264,3 +264,30 @@
 
 
 }());
+
+
+$('.more').click(function(){
+   $('.oculta').slideToggle(); 
+});
+
+
+var morelinks = document.querySelectorAll(".section > .txt-more");
+
+for (var i = 0, j = morelinks.length; i < j; i++) {
+     morelinks[0].onclick = function () {
+          var container = this.parentNode.querySelector(".container");
+          
+          container.classList.toggle("partial");
+          this.classList.toggle("txt-more");
+     };
+};
+
+
+
+
+
+
+
+
+
+
